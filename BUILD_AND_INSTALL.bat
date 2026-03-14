724d632f4bf387b5832cf81846624491f3f3db42
iTz_Nao <63752443+itznao@users.noreply.github.com>
2026-03-14 00:26:04 +0000

    Add files via upload

---
+++ BUILD_AND_INSTALL.bat
@@ -0,0 +1,10 @@
+@echo off
+IF NOT EXIST "..\..\BepInEx\interop\Assembly-CSharp.dll" (
+    echo ERROR: Launch Among Us with BepInEx installed first, then re-run this.
+    pause & exit /b 1
+)
+dotnet build -c Release
+IF %ERRORLEVEL% NEQ 0 ( pause & exit /b 1 )
+copy /Y "bin\Release\net6.0\PhantomMenu.dll" "."
+echo Done! Launch Among Us.
+pause
